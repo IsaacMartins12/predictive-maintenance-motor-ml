0dad57a011e3e2ac2ab82db4207b639a0ca9f362
isaacmartins <isaac.martins230@gmail.com>
2026-07-01 13:33:19 -0400

    fix: match isort formatting for sklearn imports

--- src/train.py
+++ src/train.py
@@ -13,13 +13,8 @@ import pandas as pd
 import yaml
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import (
-    accuracy_score,
-    classification_report,
-    f1_score,
-    precision_score,
-    recall_score,
-)
+from sklearn.metrics import (accuracy_score, classification_report, f1_score,
+                             precision_score, recall_score)
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeClassifier
